0b2cf9301c4a37f0eb7603588bdecb66f13cf7b3
admin <admin@Mac-mini-admin.local>
2023-11-26 21:48:52 +0300

    editorjs add3

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,41 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import EditorJS from "@editorjs/editorjs";
+
+const EditorComponent = () => {
+  const ejInstance = useRef();
+
+    const initEditor = () => {
+       const editor = new EditorJS({
+          holder: 'editorjs',
+          onReady: () => {
+            ejInstance.current = editor;
+          },
+          autofocus: true,
+          data: DEFAULT_INITIAL_DATA,
+          onChange: async () => {
+            let content = await editor.saver.save();
+
+            console.log(content);
+          },
+          tools: { 
+            header: Header, 
+          },
+        });
+      };
+
+      // This will run only once
+  useEffect(() => {
+    if (ejInstance.current === null) {
+      initEditor();
+    }
+
+    return () => {
+      ejInstance?.current?.destroy();
+      ejInstance.current = null;
+    };
+  }, []);
 
 export default function Home() {
   return (
@@ -15,6 +50,10 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <div id="editorjs"></div>
+        <div class="ce-example__button" id="saveButton">
+         editor.save()
+        </div>
       </main>
 
       <Footer />
